6ca55e6302f1fbd377163908436ad493c15371c6
Visionx-x <telegrambott19@gmail.com>
2024-12-07 19:30:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,14 +4,14 @@ import os
 
 
 # your bot token
-B_TOKEN = "7160882534:AAE0siO_QVDWGvJKy7tdSq93WNaBELiptzg"
+B_TOKEN = "7693118393:AAGhOrOvZNAzK_h1N20WnLElPBcDAQfiRco"
 #your api id
 API = 9333070
 # your api hash
 API_HASH = "511eb11eda4af78ec8f9a0a7de9e1241"
 # your bot username without @ like this ultron2_robot
-BOT_USERNAME = "waveapprovebot"
+BOT_USERNAME = "Nh_auto_request_bot"
 # your owner id
-ownerid = 5642016734
+ownerid = 6423243970
 #mongo db url
-DB_URI = "mongodb+srv://haris12:haris12@cluster0.u8qzvr4.mongodb.net/?retryWrites=true&w=majority"
+DB_URI = "mongodb+srv://mdnazbul:RKTHQUnOmLeHvnTn@cluster0.ayuev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
